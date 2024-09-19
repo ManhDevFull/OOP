99e31d349b479b0d7a7c94d23306fcec898348d8
SyleinIfinity <khanhsky2k5nam@gmail.com>
2024-09-19 17:12:33 +0700

    B4 - T2 - KHANH

--- Tuan2/baitap4_K.java
+++ Tuan2/baitap4_K.java
@@ -1,3 +1,5 @@
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 import java.util.*;
 public class Execrise_4{
     public static void main(String[] args) {
@@ -16,13 +18,13 @@ public class Execrise_4{
         System.out.printf("Xin moi nhap vao nam ket thuc: "); //năm kết thúc
         int NamEndALL = sc.nextInt();
         boolean Check = true;
-        int SothangALL = Hamso3(ThangBegin, Nambegin, ThangEndALL, NamEndALL);
-        System.out.println("tong so thang gui ca chu ky la: " + SothangALL);
+        int SothangALL = (int) Hamso3(ThangBegin, Nambegin, ThangEndALL, NamEndALL);
+        System.out.printf("tong so thang gui ca chu ky la: %d", SothangALL);
         double LaiTongCacChuky = Hamso1(Money, interest, ThangBegin, Nambegin, ThangEndALL, NamEndALL) - Money;
         double TongtienALLthang = Money + LaiTongCacChuky;
-        System.out.println("Tong lai ca chu ky: " + LaiTongCacChuky);
-        System.out.println("Tien goc nhan duoc cuoi chu ky: " + TongtienALLthang);
-        System.out.printf("Ban co muon rut tien giua chung ko(Yes/No): ");
+        System.out.printf("\nTong lai ca chu ky: %.2f", LaiTongCacChuky);
+        System.out.printf("\nTien goc nhan duoc cuoi chu ky: %.2f", TongtienALLthang);
+        System.out.printf("\nBan co muon rut tien giua chung ko(Yes/No): ");
         while (Check) {
             String input = sc.nextLine().trim().toLowerCase();
             if (input.equals("yes")) {
@@ -30,13 +32,13 @@ public class Execrise_4{
                 int ThangEndPART = sc.nextInt();
                 System.out.printf("Xin moi nhap vao nam ket thuc: "); //năm kết thúc giữa chu kỳ
                 int NamEndPART = sc.nextInt();
-                // Hamso2(Money, interest, ThangBegin, Nambegin, ThangEndPART, NamEndPART);
+                Hamso2(Money, interest, ThangBegin, Nambegin, ThangEndPART, NamEndPART);
                 System.out.println(" ");
-                int SothangPART = Hamso3(ThangBegin, Nambegin, ThangEndPART, NamEndPART);
+                int SothangPART = (int) Hamso3(ThangBegin, Nambegin, ThangEndPART, NamEndPART);
                 double LaiSuatNchuky = Hamso1(Money, interest, ThangBegin, Nambegin, ThangEndPART, NamEndPART) - Money;
                 double TongtienNthang = Money + LaiSuatNchuky;
-                System.out.println("Tong lai suat sau " + SothangPART + " chu ky la: " + LaiSuatNchuky);
-                System.out.println("Tien goc nhan duoc sau " + SothangPART + " chu ky la: " + TongtienNthang);
+                System.out.printf("Tong lai suat sau %d chu ky la: %.2f", SothangPART, LaiSuatNchuky);
+                System.out.printf("\nTien goc nhan duoc sau %d chu ky la: %.2f", SothangPART, TongtienNthang);
                 break;
             }
             if (input.equals("no")) {
@@ -49,7 +51,7 @@ public class Execrise_4{
     //hàm tính lãi suất kép: compound interest ~ cpi
     public static double Hamso1(double M, double In, int beginM, int beginY, int endM, int endY) {
         double LaiSuatKep = 0;
-        int SothangALL = Hamso3(beginM, beginY, endM, endY);
+        int SothangALL = (int) Hamso3(beginM, beginY, endM, endY);
         for(int i = 1; i <= SothangALL; i++){
             LaiSuatKep = M*Math.pow((1+((In/100)/12)), i);
         }        
@@ -58,7 +60,7 @@ public class Execrise_4{
     //hàm in thông tin lãi, tiền tổng hàng tháng
     public static void Hamso2(double M, double In, int beginM, int beginY, int endM, int endY) {
         double LaiSuatKep = 0;
-        int SothangPART = Hamso3(beginM, beginY, endM, endY);
+        int SothangPART = (int) Hamso3(beginM, beginY, endM, endY);
         for(int i = 1; i <= SothangPART; i++){
             LaiSuatKep = M*Math.pow((1+((In/100)/12)), i);
             System.out.println("Lai nhan duoc thang thu " + i + " la: " + LaiSuatKep);
@@ -67,32 +69,14 @@ public class Execrise_4{
         }
     }
     // Hàm đếm số tháng trong khoảng thời gian
-    public static int Hamso3(int beginM, int beginY, int endM, int endY) {
-        int totalMonths = 0;
-        for (int year = beginY; year <= endY; year++) {
-            // Xác định tháng bắt đầu và kết thúc của mỗi năm
-            int monthStart, monthEnd;
-            if (year == 0) {
-                return 0;
-            } else {
-                    if (year == beginY) {
-                        monthStart = beginM;
-                    } else {
-                        monthStart = 1;  // Từ tháng 1 của các năm sau
-                    }
-
-                    if (year == endY) {
-                        monthEnd = endM;
-                    } else {
-                        monthEnd = 12;  // Đến tháng 12 của các năm trước
-                    }
-                    
-
-                    // Đếm số tháng từ monthStart đến monthEnd
-                    totalMonths = totalMonths +  (monthEnd - monthStart + 1);
-                    break;
-            }
-        }
-        return totalMonths;
+public static long Hamso3(int beginM, int beginY, int endM, int endY) {
+        //LocalDate là 1 lớp trong java
+        //Dùng để đại diện cho ngày cụ thể (không bao gồm múi giờ trong ngày) theo định dạng yyyy-mm-dd
+        //Phương thức: of(year, month,  day): Tạo 1 dối tượng từ năm tháng cụ thể
+        LocalDate startTime = LocalDate.of(beginY, beginM, 1);
+        LocalDate endTime = LocalDate.of(endY, endM, 1);
+        //ChronoUnit là hằng số trong enum, theo đơn vị chỉ định tại hàm này là tháng
+        return ChronoUnit.MONTHS.between(startTime, endTime);
     }
+    
 }
